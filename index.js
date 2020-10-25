d5102a97c2ddeab00f5e99a244677ea5fdb5bb9e
KaliWhible <70323891+KaliWhible@users.noreply.github.com>
2020-10-25 21:03:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,63 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = 'process.env.token';
+const token = process.env.token;
+const welcomeChannelName = "인사채널";
+const byeChannelName = "인사채널";
+const welcomeChannelComment = "어서오세요.";
+const byeChannelComment = "안녕히가세요.";
 
 client.on('ready', () => {
   console.log('켰다.');
 });
 
+client.on("guildMemberAdd", (member) => {
+  const guild = member.guild;
+  const newUser = member.user;
+  const welcomeChannel = guild.channels.find(channel => channel.name == welcomeChannelName);
+
+  welcomeChannel.send(`<@${newUser.id}> ${welcomeChannelComment}\n`);
+
+  member.addRole(guild.roles.find(role => role.name == "관리자"));
+});
+
+client.on("guildMemberRemove", (member) => {
+  const guild = member.guild;
+  const deleteUser = member.user;
+  const byeChannel = guild.channels.find(channel => channel.name == byeChannelName);
+
+  byeChannel.send(`<@${deleteUser.id}> ${byeChannelComment}\n`);
+});
+
 client.on('message', (message) => {
-  if(message.content === '!로벅스 재고') {
-    message.reply('준비중');
+  if(message.author.bot) return;
+
+  if(message.content == '!재고확인') {
+    return message.reply('재고: 0 R$ / 10,000 ₩ = 1,000 R$ / 지금구입 가능');
+  }
+
+  if(message.content.startsWith('!전체공지')) {
+    if(checkPermission(message)) return
+    if(message.member != null) { // 채널에서 공지 쓸 때
+      let contents = message.content.slice('!전체공지'.length);
+      message.member.guild.members.array().forEach(x => {
+        if(x.user.bot) return;
+        x.user.send(`<@${message.author.id}> ${contents}`);
+      });
+  
+      return message.reply('공지를 전송했습니다.');
+    } else {
+      return message.reply('채널에서 실행해주세요.');
+    }
   }
 });
+
+function checkPermission(message) {
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) {
+    message.channel.send(`<@${message.author.id}> ` + "명령어를 수행할 관리자 권한을 소지하고 있지않습니다.")
+    return true;
+  } else {
+    return false;
+  }
+}
 
 client.login(token);
